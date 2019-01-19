a8eed387b74232feaf179261135c10b3a8402ced
Francis Rodrigues <francis.rod01@outlook.com>
2019-01-19 01:03:02 -0300

    Added an socket message and status

--- controller.js
+++ controller.js
@@ -1,4 +1,5 @@
 exports.index = (req, res) => {
   console.log("Controller answers with [ok].");
+  socketIO.emit('message', 'Message sent. :)');
   return res.json('ok');
 };
--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const http = require('http');
-const socket = require('socket.io');
-
 const app = express();
 const server = http.createServer(app);
-
-const controller = require('./controller');
-
-socket(server, {
+const socket = require('socket.io')(server, {
   serveClient: false,
   // below are engine.IO options
   pingInterval: 10000,
@@ -15,10 +10,18 @@ socket(server, {
   cookie: false
 });
 
+const controller = require('./controller');
+
+socket.on('connection', () => {
+  console.log('[socket] Server connected.');
+});
+
+global.socketIO = socket;
+
 app.get('/', controller.index);
 
 const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
-  console.log(`Server on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
